39a798a7d0f3ed5d7d47785d75fc3dc7d2373cc9
Andrey Vassilyev <XaveScor@gmail.com>
2018-08-05 19:41:32 +0300

    webpack-serve added. Hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 /* eslint-env node */
 
 const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	entry: path.resolve(__dirname, 'src/index.js'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle-[hash].js',
-	},
-	mode: 'development',
+    },
+    plugins: [
+		new HtmlPlugin({
+			template: 'index.html',
+		}),
+	],
+    mode: 'development',
 }
