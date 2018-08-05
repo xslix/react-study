98399730d40705e83992b9dc75a8321accb2855d
Andrey Vassilyev <XaveScor@gmail.com>
2018-08-05 19:58:51 +0300

    eslint added

--- webpack.config.js
+++ webpack.config.js
@@ -15,16 +15,24 @@ module.exports = {
 		}),
     ],
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
-            }
-        }]
+            },
+        ]
     },
     mode: 'development',
 }
