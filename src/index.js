c5db19881e7291f28fd5e21a98a07ef0a1c87189
Andrey Vassilyev <XaveScor@gmail.com>
2018-08-05 20:58:56 +0300

    react added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world')
+import * as React from 'react'
+import {render} from 'react-dom'
+
+function App() {
+	return <div>Hello World</div>
+}
+
+render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -8,31 +8,31 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle-[hash].js',
-    },
-    plugins: [
+	},
+	plugins: [
 		new HtmlPlugin({
 			template: 'index.html',
 		}),
-    ],
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "eslint-loader",
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-        ]
-    },
-    mode: 'development',
+	],
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'eslint-loader',
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+		],
+	},
+	mode: 'development',
 }
